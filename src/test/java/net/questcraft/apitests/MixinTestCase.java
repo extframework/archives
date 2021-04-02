ef1846738d6b010bd09a220a621f910a992d771f
Will Hughes <whughes98144@gmail.com>
2021-04-02 16:17:08 -0700

    simplified test case before getting bytecode

--- src/test/java/net/questcraft/apitests/MixinTestCase.java
+++ src/test/java/net/questcraft/apitests/MixinTestCase.java
@@ -5,27 +5,23 @@ import net.yakclient.mixin.api.*;
 //TODO THe VersionAdaptable just marks the annotation as not having "real" values, for the @Mixer, it means the value will actually just reference
 
 @Mixer("net.questcraft.apitests.MixinSourceClassTest")
-public abstract class MixinTestCase {
+public  class MixinTestCase {
     private int shadowInt;
 //    private String somethingElse;
 
-    @Shadow
-    public abstract void shadowMethod();
+//    @Shadow
+//    public abstract void shadowMethod();
 
-    public abstract void second();
+//    public abstract void second();
 
-    public static void staticTest() {
-        System.out.println("The current class is this");
-    }
+//    public static void staticTest() {
+//        System.out.println("The current class is this");
+//    }
 
     @Injection(to = "printTheString(I)V", type = InjectionType.BEFORE_END)
     public void overrideIt(int integer) {
-        if (integer < 10) System.out.println("THis");
+        boolean myTru = false;
+        if (myTru) System.out.println("THis");
         System.out.println("Other this");
-//        if (integer < 10) System.out.println("This");
-//        System.out.println("Other this");
-//        @NotNull String test = "THIS IS THE TEST STRING";
-//        staticTest();
-//        System.out.println("Print the test string " + test);
     }
 }
