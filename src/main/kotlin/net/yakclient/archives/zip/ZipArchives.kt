dfa1e2fe69c154c7972d064346f087bf581baa3c
Durgan <durganmcbroom@gmail.com>
2023-04-16 16:31:00 -0700

    Already, bug fixes in ZipArchives

--- src/main/kotlin/net/yakclient/archives/zip/ZipArchives.kt
+++ src/main/kotlin/net/yakclient/archives/zip/ZipArchives.kt
@@ -7,5 +7,5 @@ import net.yakclient.archives.ArchiveHandle
 public fun classLoaderToArchive(classloader: ClassLoader): ArchiveHandle = ZipHandle(
     classloader,
     classloader.definedPackages.mapTo(HashSet(), Package::getName),
-    setOf(classLoaderToArchive(classloader.parent))
+    classloader.parent?.let { setOf(classLoaderToArchive(it)) } ?: setOf()
 )
